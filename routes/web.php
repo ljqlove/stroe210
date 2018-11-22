8c09069c9678938fb038d244ec239f6ce40d67a7
ZhaoYaNing1 <1291752689@qq.com>
2018-11-22 09:50:32 +0800

    zjn@newfile

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//后台登录
+
+
+//后台管理
+Route::group([],function(){
+	//后台首页
+	Route::get('/admin', 'Admin\IndexController@index');
+
+	//后台用户管理
+	Route::resource('/admin/user','Admin\UserController');
+});
